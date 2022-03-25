c0051091092a80efc6b3782b2eba692f47f2146f
krisograbek <krzysztof.ograbek@gmail.com>
2022-03-25 12:49:14 +0100

    add react context, functions to connect wallet and checking if connected

--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -10,5 +10,58 @@ const { ethereum } = window;
 
 const getEthereumContract = () => {
   const provider = new ethers.providers.Web3Provider(ethereum);
+  const signer = provider.getSigner();
+  const transactionContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+  console.log({ provider, signer, transactionContract });
 }
 
+export const TransactionProvider = ({ children }) => {
+  const [connectedAccount, setConnectedAccount] = useState('');
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return alert("Please install metamask");
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      setConnectedAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+
+      throw new Error("No Ethereum Object")
+    }
+  }
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+
+      if (!ethereum) return alert("Please install metamask");
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      console.log(accounts)
+
+      if (accounts.length) {
+        setConnectedAccount(accounts[0]);
+
+        // get all transactions
+      } else {
+        console.log("No accounts found")
+      }
+    } catch (error) {
+      console.log(error)
+
+      throw new Error("No Ethereum Object")
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+
+
+  return (
+    <TransactionContext.Provider value={{ connectWallet }}>
+      {children}
+    </TransactionContext.Provider>
+  )
+}
